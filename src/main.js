2a572d149c4288317c0120240a617ce915169852
yanling-yongwei <2895096691@qq.com>
2021-01-14 11:34:06 +0800

    下载依赖包

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import "./assets/js/rem"
+import "./assets/css/reset.css"; 
+import "./assets/css/common.css"
+
+import Vant from "vant";
+import "vant/lib/index.css";
+Vue.use(Vant);
+
+import axios from "axios";
+Vue.prototype.$axios=axios;
+
 new Vue({
   router,
   store,
