4f2a17f16babc375d4c5b4787ab4c340697c7ea1
yanling-yongwei <2895096691@qq.com>
2021-01-15 19:40:40 +0800

    我的页面优化

--- src/views/My/router.js
+++ src/views/My/router.js
@@ -1,5 +1,4 @@
-export default [
-  {
+export default [{
     path: "/info",
     name: "name",
     component: () => import("./Info.vue"),
@@ -14,4 +13,29 @@ export default [
     name: "calendar",
     component: () => import("./Calendar.vue"),
   },
+  {
+    path: "/collect",
+    name: "collect",
+    component: () => import('./Collect.vue')
+  },
+  {
+    path: "/attention",
+    name: "attention",
+    component:()=>import('./Attention.vue')
+  },
+  {
+    path:'/opinion',
+    name:"opinion",
+    component:()=>import('./Opinion.vue')
+  },
+  {
+    path:'/learn',
+    name:"learn",
+    component:()=>import('./Learn.vue')
+  },
+  {
+    path: '/discounts',
+    name: "discounts",
+    component:()=>import('./Discounts.vue')
+  }
 ]
