f9270ca9e82c2350b2b494c6d4c9e868f9e182d1
刘立奇 <2245815284@qq.com>
2021-01-15 15:24:49 +0800

    1-15

---
+++ src/views/HomeFeature/ejs.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        path:"/Feature",
+        name:"Feature",
+        component:()=>import("./Feature")
+    },
+    {
+        path:"/Tutorship",
+        name:"Tutorship",
+        component:()=>import("./Tutorship")
+    },
+    {
+        path:"/Calendar",
+        name:"Calendar",
+        component:()=>import("./Calendar")
+    }
+]
