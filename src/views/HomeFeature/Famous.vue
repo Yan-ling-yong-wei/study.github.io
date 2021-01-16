0e3409f8217052ffa5e87aa67b475fdaac91ddde
刘立奇 <2245815284@qq.com>
2021-01-16 21:04:58 +0800

    1-16

---
+++ src/views/HomeFeature/Famous.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Famous_container">
+    <div><Header title="名师" back /></div>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/Header";
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      active: 1,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.Famous_container {
+  width: 100%;
+  height: 100%;
+}
+</style>
